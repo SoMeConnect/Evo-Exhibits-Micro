d5752191eeda14cdf99bb107db56011172de2a16
James Burrell <jamesburrell@SoMes-iMac.local>
2017-10-11 12:35:40 -0500

    Removed hr tags from bottom two video items

--- parts/slides/03.php
+++ parts/slides/03.php
@@ -35,13 +35,11 @@
                 <h3><a href="https://www.youtube.com/watch?v=iTb_Pmii4Ac" rel="prettyPhoto" target="new">Discover. Create. Connect. Part 3 of our DCC Series</a></h3>
                 <p>Connecting to serve means, our team is your team! 
                 Watch our video and get to know Evo Exhibits teammates. <a class="readMore" href="https://www.youtube.com/watch?v=iTb_Pmii4Ac" rel="prettyPhoto" target="new">Watch the Video <em class="fa fa-angle-right"></em></a></p>
-                <hr class="hrNews">
             </div>
             <div class="col-md-6 text-center youtube">
                 <a href="https://www.linkedin.com/company/329147/" target="new" style="border: none; text-decoration: none;"><img class="img-responsive picsGall" src="wp-content/themes/evo-microsite/assets/images/picNews/news_Inc500.jpg"/></a>
-                <h3><a href="https://www.linkedin.com/feed/update/urn:li:activity:6317094052500246528" target="new">Evo Exhibits made the Inc5000 list for the 5th time!</a></h3>
+                <h3><a href="https://www.linkedin.com/feed/update/urn:li:activity:6317094052500246528" target="new">Evo Exhibits made the Inc. 5000 list for the 5th time!</a></h3>
                 <p>Go to our LinkedIn page and stay up to date on the latest. <a class="readMore" href="https://www.linkedin.com/company/329147/" target="new">Connect with Us! <em class="fa fa-angle-right"></em></a></p>
-                <hr class="hrNews">
             </div>
         </div>
     </div>
